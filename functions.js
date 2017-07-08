afa6fa7e115d3f0fe599992498b6a4d71597965f
Deimantas Abelskis <ddeimantass@gmail.com>
2017-07-08 16:15:48 +0300

    functionality

---
+++ functions.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $("#evernote, #warner, #findlaw, #livenation, #nhl, #bethesda").click(function(){
+        slide($(this));
+    });
+    setInterval(slideShow,5000);
+});
+function slide(that){
+    $("#meniu ul li.active").removeClass("active");
+    that.addClass("active");
+    $("#karusele .container").addClass("hide");
+    var id = that.attr("id");
+    $("#"+id+"Container").removeClass("hide");
+}
+function slideShow(){
+    switch($("#meniu ul li.active").attr("id")){
+        case "evernote": $("#warner").trigger("click"); break;
+        case "warner": $("#findlaw").trigger("click"); break;
+        case "findlaw": $("#livenation").trigger("click"); break;
+        case "livenation": $("#nhl").trigger("click"); break;
+        case "nhl": $("#bethesda").trigger("click"); break;
+        case "bethesda": $("#evernote").trigger("click"); break;
+    }
+}
